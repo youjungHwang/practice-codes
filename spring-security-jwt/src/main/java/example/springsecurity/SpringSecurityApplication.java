0a2845e2414d992e235fc041d3bfbff8166af0c4
youjungHwang <dbwjd5032@gmail.com>
2022-08-05 18:07:24 +0900

    add log

--- spring-security-jwt/src/main/java/example/springsecurity/SpringSecurityApplication.java
+++ spring-security-jwt/src/main/java/example/springsecurity/SpringSecurityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
+		System.out.println("update");
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
