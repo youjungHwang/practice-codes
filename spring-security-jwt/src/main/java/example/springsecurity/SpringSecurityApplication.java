4014c13fdb9922568a410f5a8c5b7c185d492a34
youjungHwang <dbwjd5032@gmail.com>
2022-08-05 18:05:07 +0900

    delete log

--- spring-security-jwt/src/main/java/example/springsecurity/SpringSecurityApplication.java
+++ spring-security-jwt/src/main/java/example/springsecurity/SpringSecurityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
-		System.out.println("spring boot");
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
